12f30d6f0cef770691249f2aee82c314c7baa659
shard520 <65344196+shard520@users.noreply.github.com>
2021-11-15 17:03:28 +0000

    Add get, patch, and delete routes for user.

--- src/user/user.controllers.js
+++ src/user/user.controllers.js
@@ -1,10 +1,59 @@
+const { save } = require('mongoose');
+
 const User = require('./user.model');
 
 exports.addUser = async (req, res) => {
   try {
     const newUser = new User(req.body);
+
     await newUser.save();
     res.status(200).send({ message: 'Success' });
+  } catch (err) {
+    console.error('💥 💥', err);
+    res
+      .status(418)
+      .send({ message: 'Something went wrong, check server logs.' });
+  }
+};
+
+exports.listUsers = async (req, res) => {
+  try {
+    const userResponse = await User.find({});
+
+    res.status(200).send(userResponse);
+  } catch (err) {
+    console.error('💥 💥', err);
+    res
+      .status(418)
+      .send({ message: 'Something went wrong, check server logs.' });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const doc = await User.findOne({ username: req.body.update.username });
+
+    const { newInfo } = req.body;
+
+    if (newInfo.username) doc.username = newInfo.username;
+    if (newInfo.email) doc.email = newInfo.email;
+    if (newInfo.password) doc.password = newInfo.password;
+
+    await doc.save();
+
+    res.status(200).send({ message: 'Update successful' });
+  } catch (err) {
+    console.error('💥 💥', err);
+    res
+      .status(418)
+      .send({ message: 'Something went wrong, check server logs.' });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.deleteOne(req.body);
+    res.status(200).send({ message: 'Deletion successful' });
   } catch (err) {
     console.error('💥 💥', err);
     res
--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,7 +1,18 @@
 const { Router } = require('express');
-const { addUser } = require('./user.controllers');
+const {
+  addUser,
+  listUsers,
+  updateUser,
+  deleteUser,
+} = require('./user.controllers');
 const userRouter = Router();
 
 userRouter.post('/user', addUser);
+
+userRouter.get('/user', listUsers);
+
+userRouter.patch('/user', updateUser);
+
+userRouter.delete('/user', deleteUser);
 
 module.exports = userRouter;
